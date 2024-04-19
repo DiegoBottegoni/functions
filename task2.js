f1dcecaccce4739ef83d19cb266ac2171da1a2bc
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2024-04-19 15:23:07 -0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,4 +1,18 @@
-const getFullName = person => `${person.firstName} ${person.lastName}`;
+const getFullName = (person) => {
+    try {
+      if (typeof person !== 'object' || person === null || Array.isArray(person)) {
+        throw new Error('The argument must be a valid object.');
+      }
+  
+      if (typeof person.firstName !== 'string' || typeof person.lastName !== 'string') {
+        throw new Error('The object must contain the firstName and lastName properties as text strings.');
+      }
+  
+      return `${person.firstName} ${person.lastName}`;
+    } catch (error) {
+      return `Error: ${error.message}`;
+    }
+  };
 
 const filterUniqueWords = text =>
 text.split(/\s+/)
@@ -11,10 +25,18 @@ students.map(student => student.grades.reduce((total, grade) => total + grade, 0
 
 // TESTING //
 
+console.log("***********************************")
+console.log("getFullName:");
 const person = { firstName: "Diego", lastName: "Bottegoni" };
 console.log(getFullName(person));
+console.log("getFullName error cases:")
+const person2 = { firstName: 1, lastName: true};
+console.log(getFullName(person2));
+const person3 = [ firstName = "Diego", lastName = "Bottegoni"];
+console.log(getFullName(person3));
+console.log("***********************************")
 
-const text = "apple banana banana orange apple";
+const text = "apple Banana banana orange apple";
 console.log(filterUniqueWords(text));
 
 const students = [
@@ -23,3 +45,4 @@ const students = [
 { name: "Charlie", grades: [75, 80, 85] }
 ];
 console.log(getAverageGrade(students));
+
